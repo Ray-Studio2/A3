49fed77a4995a8ddb2468d897245ede3ac971213
chaeyoon-lee-rae <engrrachel318@gmail.com>
2025-07-26 14:30:50 +0900

    Update submodule

--- A3/RenderSettings.h
+++ A3/RenderSettings.h
@@ -13,11 +13,12 @@ struct RenderSettings
 
 	static constexpr uint32 maxLightCounts = 16;
 
-	static constexpr const char* sceneFiles[] = { "../Assets/bruteforce-local.json",
-												 "../Assets/nee-local.json",
-												 "../Assets/bruteforce-env.json",
-												 "../Assets/nee-env.json",
-												 "../Assets/nee-env2.json" };
+	static constexpr const char* sceneFiles[] = {//"../Assets/bruteforce-local.json",
+												//  "../Assets/nee-local.json",
+												//  "../Assets/bruteforce-env.json",
+												//  "../Assets/nee-env.json",
+												//  "../Assets/nee-env2.json",
+												 "../Assets/nee-disney.json" };
 	static constexpr uint32 sceneIdx = 0;
 
 	// static constexpr const char* envMapDefault = "../Assets/reichstag_1_4k.hdr";
